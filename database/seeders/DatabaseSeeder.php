5f1fe7ad820eaf108af24e204681899e383958a8
Ahsanul Houqe <ahsanan@outlook.com>
2024-02-26 19:26:19 +0600

    added scope popular and highest_rated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +14,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        Book::factory(33)->create()->each(function ($book){
+            $numberReviews = random_int(5,30);
+
+            Review::factory()->count($numberReviews)
+                ->good()
+                ->for($book)
+                ->create();
+        });
+
+
+        Book::factory(33)->create()->each(function ($book){
+            $numberReviews = random_int(5,30);
+
+            Review::factory()->count($numberReviews)
+                ->average()
+                ->for($book)
+                ->create();
+        });
+
+
+        Book::factory(34)->create()->each(function ($book){
+            $numberReviews = random_int(5,30);
+
+            Review::factory()->count($numberReviews)
+                ->bad()
+                ->for($book)
+                ->create();
+        });
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
